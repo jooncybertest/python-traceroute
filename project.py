291d8f71f05effc28433e8dcad05cb304804d4bc
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-09-18 17:12:33 -0400

    Update project.py

--- project.py
+++ project.py
@@ -34,7 +34,6 @@ def find_and_plot_coordinates(hostname):
             # Initialize Google Maps plotter
             gmap = gmplot.GoogleMapPlotter(0, 0, 3)
 
-            
             ips = []
 
             # Extract IP addresses from the traceroute results
